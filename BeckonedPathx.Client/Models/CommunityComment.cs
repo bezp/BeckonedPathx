f7398c88c64f0bf6ae1aa5d433cd6077d2f1a88b
bezp <bezalelp@uci.edu>
2018-08-11 21:43:39 -0400

    commented out db initializer

---
+++ BeckonedPathx.Client/Models/CommunityComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeckonedPathx.Client.Models
+{
+    public class CommunityComment
+    {
+        public int CommunityCommentId { get; set; }
+
+
+
+    }
+}
---
+++ BeckonedPathx.Client/Models/Friend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeckonedPathx.Client.Models
+{
+    public class Friend
+    {
+        public int FriendId { get; set; }
+
+
+
+
+
+    }
+}
